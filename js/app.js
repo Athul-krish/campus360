74a5a2168c89c374bc0d1933cb1057cc00820326
Athul-krish <athulkrishnapanamakkal@gmail.com>
2026-07-22 11:38:01 +0530

    added select tags

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 var app = angular.module("campus360", []);
 
 app.controller("dashboardcontroller", function ($scope) {
-  $scope.collegename = "FISAT";
-  $scope.totalstudents = 4200;
+  $scope.collegename = "FiSaT";
+  $scope.totalstudents = 1200;
   $scope.totalfaculty = 85;
+  $scope.fees = 19500;
+  $scope.today = new Date();
+  $scope.departments=["CSE","MCA","MBA","EEE","BCA","IMCA"];
+  $scope.selectedDepartment="MCA"
   console.log("dashboard controller load aayind");
 
   $scope.students = ["Luka", "John", "Jane", "Bob"];
