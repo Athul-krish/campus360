5db7d38f0dcb93c3c7838fd86b14223b6d61d961
Athul-krish <athulkrishnapanamakkal@gmail.com>
2026-07-23 11:50:16 +0530

    added form validations

--- js/app.js
+++ js/app.js
@@ -7,28 +7,32 @@ app.controller("dashboardcontroller", function ($scope) {
   $scope.fees = 19500;
   $scope.today = new Date();
   $scope.departments=["CSE","MCA","MBA","EEE","BCA","IMCA"];
-  $scope.selectedDepartment="MCA"
-
+  $scope.selectedDepartment="MCA";
   $scope.isDisabled = true;
+
+  
   $scope.allowAdmission = function(){
-    $scope.isDisabled=false;
+    $scope.isDisabled = false;
   };
 
-  $scope.readOnly=true;
+
+  
+  $scope.readOnly = true;
   $scope.toggleReadOnly=function(){
     $scope.readOnly=!$scope.readOnly;
   };
 
 
-//to check if this is working
-  console.log("dashboard controller load aayind");
+  // to check if this is working
+  console.log("dashboard controller loaded");
 
   $scope.students = ["Luka", "John", "Jane", "Bob"];
+  $scope.showstudents = false;
+  $scope.showDashboard = false;
 
   $scope.addstudent = function () {
     $scope.totalstudents++;
     $scope.showstudents = true;
-
     $scope.showDashboard = true;
   };
 });
